fa1e31e889417e8cb80a9dde7d76dcfd1157f7a0
“Tajinder” <tajinder.lamba@sedna.com>
2019-08-02 09:25:56 -0700

    SED-6183 tests fix

--- test/style_parser/namespacer/prefixer.js
+++ test/style_parser/namespacer/prefixer.js
@@ -18,12 +18,13 @@ describe('lib - html purifier - style parser - namespacer - prefixer', function(
 
   describe('prefix', function() {
     var PREFIX = 'ugc-';
+    const messageId = "";
 
     it('should add the prefix to just a class', function(done) {
       var dirty = '.link {margin-top:0;}';
-      var clean = '.ugc-link {margin-top:0;}';
+      var clean = '.ugc-link {\n  margin-top: 0;\n}';
 
-      prefixer.prepend(dirty, PREFIX, function(err, prefixed) {
+      prefixer.prepend(dirty, PREFIX, null, messageId, function(err, prefixed) {
         expect(prefixed).to.equal(clean);
         done();
       });
@@ -31,19 +32,19 @@ describe('lib - html purifier - style parser - namespacer - prefixer', function(
 
     it('should add the prefix to a tag and class', function(done) {
       var dirty = 'p.orig {margin-top:0;}';
-      var clean = 'p.ugc-orig {margin-top:0;}';
+      var clean = 'p.ugc-orig {\n  margin-top: 0;\n}';
 
-      prefixer.prepend(dirty, PREFIX, function(err, prefixed) {
+      prefixer.prepend(dirty, PREFIX, null, messageId, function(err, prefixed) {
         expect(prefixed).to.equal(clean);
         done();
       });
     });
 
     it('should add the prefix with multiple tags', function(done) {
-      var dirty = '.first.second.third {margin-top:0;}';
-      var clean = '.ugc-first.ugc-second.ugc-third {margin-top:0;}';
+      var dirty = '.first .second .third {margin-top:0;}';
+      var clean = '.ugc-first .ugc-second .ugc-third {\n  margin-top: 0;\n}';
 
-      prefixer.prepend(dirty, PREFIX, function(err, prefixed) {
+      prefixer.prepend(dirty, PREFIX, null, messageId, function(err, prefixed) {
         expect(prefixed).to.equal(clean);
         done();
       });
@@ -51,9 +52,9 @@ describe('lib - html purifier - style parser - namespacer - prefixer', function(
 
     it('should add the prefix with multiple class blocks', function(done) {
       var dirty = 'p.orig, div.test .link {margin-top:0;}';
-      var clean = 'p.ugc-orig, div.ugc-test .ugc-link {margin-top:0;}';
+      var clean = 'p.ugc-orig,\ndiv.ugc-test .ugc-link {\n  margin-top: 0;\n}';
 
-      prefixer.prepend(dirty, PREFIX, function(err, prefixed) {
+      prefixer.prepend(dirty, PREFIX, null, messageId, function(err, prefixed) {
         expect(prefixed).to.equal(clean);
         done();
       });
@@ -61,9 +62,9 @@ describe('lib - html purifier - style parser - namespacer - prefixer', function(
 
     it('should add the prefix to an id', function(done) {
       var dirty = '#orig {margin-top:0;}';
-      var clean = '#ugc-orig {margin-top:0;}';
+      var clean = '#ugc-orig {\n  margin-top: 0;\n}';
 
-      prefixer.prepend(dirty, PREFIX, function(err, prefixed) {
+      prefixer.prepend(dirty, PREFIX, null, messageId, function(err, prefixed) {
         expect(prefixed).to.equal(clean);
         done();
       });
@@ -71,9 +72,9 @@ describe('lib - html purifier - style parser - namespacer - prefixer', function(
 
     it('should add the prefix to id with tag', function(done) {
       var dirty = 'p#orig {margin-top:0;}';
-      var clean = 'p#ugc-orig {margin-top:0;}';
+      var clean = 'p#ugc-orig {\n  margin-top: 0;\n}';
 
-      prefixer.prepend(dirty, PREFIX, function(err, prefixed) {
+      prefixer.prepend(dirty, PREFIX, null, messageId, function(err, prefixed) {
         expect(prefixed).to.equal(clean);
         done();
       });
@@ -81,9 +82,9 @@ describe('lib - html purifier - style parser - namespacer - prefixer', function(
 
     it('should treat id that is a valid tag name as an id name', function(done) {
       var dirty = '#div {margin-top:0;}';
-      var clean = '#ugc-div {margin-top:0;}';
+      var clean = '#ugc-div {\n  margin-top: 0;\n}';
 
-      prefixer.prepend(dirty, PREFIX, function(err, prefixed) {
+      prefixer.prepend(dirty, PREFIX, null, messageId, function(err, prefixed) {
         expect(prefixed).to.equal(clean);
         done();
       });
@@ -91,9 +92,9 @@ describe('lib - html purifier - style parser - namespacer - prefixer', function(
 
     it('should prefix multiple', function(done) {
       var dirty = '#link ,#link2 #link3 #link4 #link5, p#test {margin-top:0;}';
-      var clean = '#ugc-link ,#ugc-link2 #ugc-link3 #ugc-link4 #ugc-link5, p#ugc-test {margin-top:0;}';
+      var clean = '#ugc-link,\n#ugc-link2 #ugc-link3 #ugc-link4 #ugc-link5,\np#ugc-test {\n  margin-top: 0;\n}';
 
-      prefixer.prepend(dirty, PREFIX, function(err, prefixed) {
+      prefixer.prepend(dirty, PREFIX, null, messageId, function(err, prefixed) {
         expect(prefixed).to.equal(clean);
         done();
       });
